4a21ea499bf896467253faae9779abfbcbd0a3fc
tamirgolan <gotamirgolan@gmail.com>
2022-08-17 13:30:53 +0300

    gg

---
+++ system driil.py
@@ -0,0 +1,10 @@
+
+system_requwest=int(input("please chouse number:"))
+
+for i in range(1,system_requwest):
+    print(system_requwest)
+    if system_requwest>system_requwest:
+        print("biger")
+
+
+
